54931fe7bcd0ea662dc210e9d60138f6eace7596
phob0z <leonel.alfonso@gmail.com>
2022-06-01 18:06:42 -0500

    Holi

---
+++ app/Models/Portafolio.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Portafolio extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'categoria',
+        'imagen',
+        'url'
+    ];
+}
